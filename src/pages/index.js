07e049389bb8ba2f8093141d747a5ea96118f61a
jawnchuks <jawnchuks@gmail.com>
2022-04-01 13:18:19 +0100

    fixed linting errors

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head"
-import Image from "next/image"
 import styles from "../styles/Home.module.css"
 
 export default function Home() {
@@ -11,9 +10,7 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main>
-				CMS website
-		</main>
+			<main>CMS website</main>
 		</div>
 	)
 }
