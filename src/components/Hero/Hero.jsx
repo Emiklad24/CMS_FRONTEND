8fad4d0af50e944cd71e8be45d9e2a98d6c92be9
jawnchuks <jawnchuks@gmail.com>
2022-04-01 13:21:30 +0100

    commit to switch branch

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,35 +1,33 @@
-import Image from "next/image";
-import Link from "next/link";
-import HeroImg from "../../assets/images/heroImg.svg";
+import Image from "next/image"
+import Link from "next/link"
+import HeroImg from "../../assets/images/heroImg.svg"
 
 const Hero = () => {
-  return (
-    <div className="bg-background-bg w-full md:py-0 py-8">
-      <div className="w-[90vw] m-auto flex flex-col-reverse md:flex-row justify-between items-center">
-        <div className="hero-text basis-1/2 flex flex-col md:py-16 items-left justify-center">
-          <h1 className="title text-2xl md:text-4xl lg:text-5xl md:text-left text-center text-dark-gray  font-bold xl:w-3/4">
-            DISCOVER AND TRACK <br />
-            <span className="text-blue-primary">CONSULTANTS</span> <br />
-            ACROSS THE GLOBE
-          </h1>
-          <h3 className="descr text-xs md:text-xl my-4 md:my-8 text-center md:text-left">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </h3>
-          <Link href="/signup">
-           <a
-            className="w-2/3 md:w-2/4 px-4  py-2 md:py-4 bg-blue-primary text-white uppercase text-sm md:text-md m-auto md:ml-0 text-center"
-          >
-            Let&apos;s get started testing stuff
-          </a>
-          </Link>
-        </div>
-        <div className="hero-img basis-1/2">
-          <Image src={HeroImg} alt="" />
-        </div>
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className="bg-background-bg w-full md:py-0 py-8">
+			<div className="w-[90vw] m-auto flex flex-col-reverse md:flex-row justify-between items-center">
+				<div className="hero-text basis-1/2 flex flex-col md:py-16 items-left justify-center">
+					<h1 className="title text-2xl md:text-4xl lg:text-5xl md:text-left text-center text-dark-gray  font-bold xl:w-3/4">
+						DISCOVER AND TRACK <br />
+						<span className="text-blue-primary">CONSULTANTS</span> <br />
+						ACROSS THE GLOBE
+					</h1>
+					<h3 className="descr text-xs md:text-xl my-4 md:my-8 text-center md:text-left">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.
+					</h3>
+					<Link href="/signup">
+						<a className="w-2/3 md:w-2/4 px-4  py-2 md:py-4 bg-blue-primary text-white uppercase text-sm md:text-md m-auto md:ml-0 text-center">
+							Let&apos;s get started testing stuff
+						</a>
+					</Link>
+				</div>
+				<div className="hero-img basis-1/2">
+					<Image src={HeroImg} alt="" />
+				</div>
+			</div>
+		</div>
+	)
+}
 
-export default Hero;
+export default Hero
